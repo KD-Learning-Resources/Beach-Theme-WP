dd5a7d44366a8aaf15e329c2c5f46f300a4d7516
Katrina Dierking <kalynndier@gmail.com>
2021-02-05 22:55:15 -0500

    initial commit

---
+++ wp-content/plugins/Rocket-helper-plugin/rocket-helper-plugin.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @package RocketHelperPlugin
+ */
+/*
+Plugin Name: Rocket Helper Plugin
+Plugin URI: https://hertimetocode.com/plugin
+Description: Helper function to prevent WP Rocket from displacing errors.
+Version: 1.0.0
+Author: Katrina Dierking | Her Time to Code
+Author URI: https://hertimetocode.com
+License: GPLv2 or later
+Text Domain: rocket-helper-plugin
+*/
